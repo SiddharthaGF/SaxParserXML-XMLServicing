a2d60a79686fb107862eb50abbf854ff68d3ca69
SiddharthaGF <pablo.guerra@espoch.edu.ec>
2021-01-04 22:13:14 -0500

    version 3.0

--- src/main/java/xmlsaxparser_xmlservicing/App.java
+++ src/main/java/xmlsaxparser_xmlservicing/App.java
@@ -1,7 +1,18 @@
 package main.java.xmlsaxparser_xmlservicing;
 
+/*
+    author: @pablo_guerra_6502, @andre_robalino_6481
+*/
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        SaxParser sp = new SaxParser("https://guarded-springs-28309.herokuapp.com/employees");
+        try 
+        {
+            sp.parse();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
